226d28d4393acf0a67f0365d5c56d1f2d6c36935
LuizFelipe406 <luizfpereira406@gmail.com>
2022-11-03 15:46:40 -0300

    ajuste docker

--- backend/src/database/migrations/20221103181242-create-users.js
+++ backend/src/database/migrations/20221103181242-create-users.js
@@ -1,33 +1,36 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-export async function up(queryInterface, Sequelize) {
-  await queryInterface.createTable('Users', {
-    id: {
-      primaryKey: true,
-      allowNull: false,
-      type: Sequelize.INTEGER
-    },
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    pictureUrl: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    }
-  });
-}
-export async function down(queryInterface, Sequelize) {
-  await queryInterface.dropTable('Users');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.INTEGER 
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pictureUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+})
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
 }
