664b3b00f814878a419cd016b5dd78401ef619b5
LuizFelipe406 <luizfpereira406@gmail.com>
2022-11-11 11:36:55 -0300

    Estrutura inicial das rotas

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Login() {
+
+    return(
+        <div className="login-container">
+            <input type="text" />
+            <input type="text" />
+            <button></button>
+        </div>
+    );
+}
+
+export default Login
---
+++ frontend/src/pages/Register/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Register() {
+    return(
+        <div className="register-container">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <button>Cadastrar</button>
+        </div>
+    );
+}
+
+export default Register;
